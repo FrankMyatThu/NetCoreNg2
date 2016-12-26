f6bbfad76502b923cbfec47139f9a293222123fe
frank.myatthu <myatthu1986@gmail.com>
2016-12-26 16:45:55 +0800

    asp.net core web api, set httpOnly cookie and get it back

---
+++ NetCore/CookieTester/WebApplication1/src/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [Route("GenerateToken")]
+        public IActionResult GenerateToken()
+        {
+            string _JwtTokenDummy = Guid.NewGuid().ToString() + " Developer testing";
+            Response.Cookies.Append(
+                "JWTToken",
+                _JwtTokenDummy,
+                new Microsoft.AspNetCore.Http.CookieOptions()
+                {
+                    Path = "/",
+                    HttpOnly = true,
+                    Secure = false,
+                }
+            );
+            return new ObjectResult("GenerateToken._JwtTokenDummy: " + _JwtTokenDummy);
+        }
+
+        [HttpGet]
+        [Route("RetrieveToken")]
+        public IActionResult RetrieveToken()
+        {   
+            return new ObjectResult("RetrieveToken._JwtTokenDummy: " + Request.Cookies["JWTToken"].ToString());
+        }
+    }
+}
---
+++ NetCore/CookieTester/WebApplication1/src/WebApplication1/Helper/XNetCoreHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Helper
+{
+    public static class XNetCoreHelper
+    {
+        internal static void SetCookie(this HttpResponseHeaders headers, Cookie cookie)
+        {           
+            var cookieBuilder = new StringBuilder(WebUtility.UrlEncode(cookie.Name) + "=" + WebUtility.UrlEncode(cookie.Value));
+            if (cookie.HttpOnly)
+            {
+                cookieBuilder.Append("; HttpOnly");
+            }
+
+            if (cookie.Secure)
+            {
+                cookieBuilder.Append("; Secure");
+            }
+
+            headers.Add("Set-Cookie", cookieBuilder.ToString());
+        }
+    }
+}
