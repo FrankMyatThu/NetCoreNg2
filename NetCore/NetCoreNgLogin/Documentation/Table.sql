4abed10b746da9cf78514edc4325b0685dbc8e96
willow <willow>
2017-01-04 16:16:54 +0800

    When user inactive for x minutes,

--- NetCore/NetCoreNgLogin/Documentation/Table.sql
+++ NetCore/NetCoreNgLogin/Documentation/Table.sql
@@ -17,17 +17,20 @@ tbl_UserDetail
 ClientBrowser
 	<body onload="onInactive(5000, logout);"></body>
 	<script>
-	var ExpiredAt = xx/xx/xxxx 8:15 AM;
-	var TimeToBeRefresh = GetTotalMilliSecond(GetTotalMinutes(ExpiredAt - CurrentTime));
-	var MaxAllowedTimeToBeIdle = TimeToBeRefresh;
-	setTimeout(JWTTokenRefresher, TimeToBeRefresh);	
-	onInactive(MaxAllowedTimeToBeIdle, logout);
+	/// Start - Token refresh logic 
+	var ExpiredAt = xx/xx/xxxx 8:15 AM;	
+	var TimeToBeRefresh = GetTotalMilliSecond(GetTotalMinutes(ExpiredAt - CurrentTime));	
+	setTimeout(JWTTokenRefresher, TimeToBeRefresh);		
 	function JWTTokenRefresher(){
 		// Replace JWTToken in background without notifying user.
 		// update 
-			// TimeToBeRefresh
-			// MaxAllowedTimeToBeIdle
+			// cleartimeout and settimeout again for TimeToBeRefresh			
 	}
+	/// End - Token refresh logic 
+	
+	/// Start - session time out logic
+	var MaxAllowedTimeToBeIdle = xxxxx;
+	onInactive(MaxAllowedTimeToBeIdle, logout);
 	function onInactive(millisecond, callback){
 		var wait = setTimeout(callback, millisecond);               
 		document.onmousemove = 
@@ -43,6 +46,14 @@ ClientBrowser
 	function logout(){
 		console.log('Logout');
 	}
+	/// End - session time out logic
 	</script>
+
+[Note] 
+According to upper scenario, 
+When user inactive for x minutes, 
+then session can be time out 
+even if background process received new token 
+within 1 second before time out.
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------
